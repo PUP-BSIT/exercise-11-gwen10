44b50b8f1c6d940890e517490aad17968aba73c3
gonato-janalexalouissee <alexagonato@gmail.comm>
2025-05-14 10:40:54 +0800

    Add gonato.py.

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from gwen10package.bataller import generate_random_integers
 from gwen10package.sario import tell_joke
 from gwen10package.teves import fake_profile
 from gwen10package.miguel import check_internet
+from gwen10package.gonato import random_person_info
 
 generate_random_integers(10, 1, 100)
 tell_joke()
 fake_profile()
 check_internet()
+random_person_info()
